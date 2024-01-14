4f395f85acce8438950a9ccb094f565101f61643
JIN\xkr12 <xkr123456@gmail.com>
2024-01-14 13:01:07 +0900

    please

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'AI_Chat.html'));
 });
 
-app.use(express.static('public'));
+app.use("/public" , express.static('./public/'));
 app.use((req, res, next) => {
   res.status(404).send('Not Found');
 });
