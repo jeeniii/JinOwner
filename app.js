bdffb05828d3b82c3101296cba50e265ac7bbb7c
JIN\xkr12 <xkr123456@gmail.com>
2024-01-14 22:14:03 +0900

    agsdds

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'AI_Chat.html'));
 });
 
-app.use("/public" , express.static('./public/'));
+app.use('/public' , express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   res.status(404).send('Not Found');
 });
